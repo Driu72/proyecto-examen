5c43eb80b2899bab1b8476cd30a9ebc363091a33
Andrea <205613407+Driu72@users.noreply.github.com>
2025-05-28 09:23:24 +0200

    Intento 2 de que haya conflinto en los cambios

--- modulo1.py
+++ modulo1.py
@@ -2,6 +2,6 @@ import os
 print('Modulo 1 del Proyecto App')
 input()
 os.system('cls')
-print('Impresiones por pantalla para ver los cambios')
+print('Impresiones por pantalla para ver todos los cambios')
 input()
 os.system('cls')
